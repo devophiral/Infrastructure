307062245f392fa17286494e92385a80f8c8c6e4
hirals <45hiral.solanki@gmail.com>
2023-11-05 19:27:11 +0530

    resolving errors locally

--- global/my-web-app/staging/global_variables.tfvars
+++ global/my-web-app/staging/global_variables.tfvars
@@ -1,19 +1,15 @@
-variable "global" {
-  type = object({
+global = {
     country	= "IN"
     environment_name	= "staging"
     organization	= "Infopercept"
     region	= "us-west-2"
     application_name  = "demo-app"
-    vpc_id	=  my_vpc
     cidr_blocks	= ["0.0.0.0/0"]
     public_cidr_block = ["10.0.1.0/24"]
     private_cidr_block = ["10.0.2.0/24"]
     vpc_cidr = ["10.0.0.0/16"]
-    IAM_Role	= 
-    keypair = "/home/lenovo/Downloads/test.pem"
+    keypair = "/home/lenovo/Downloads/test1.pem"
     az1 = "us-west-2a"
     az2 = "us-west-2b"
     policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkFullAccess"
-})
 }
--- global/my-web-app/staging/main.tf
+++ global/my-web-app/staging/main.tf
@@ -4,24 +4,36 @@ provider "aws" {
 
 module "iam" {
   source        = "../../../modules/iam"  # Path to IAM module
+  global = var.global
 }
 module "vpc" {
   source = "../../../modules/vpc"  # Path to VPC module
+  global = var.global
 }
 
 module "elasticbeanstalk" {
+  global = var.global
   source    = "../../../modules/elasticbeanstalk"  # Path to Elastic Beanstalk module
 }
 module "alb" {
+  global = var.global
   source        = "../../../modules/alb"  # Path to ALB module
+  subnet_id = module.vpc.public_subnet_id.id
 }
 module "ec2-autoscaller" {
   source        = "../../../modules/ec2-autoscaller"  # Path to Autoscaller module
+  global = var.global
+  input = {
+      image_id = "ami-02643bbd3f82ce3b5"
+      instance_type = "t2.micro"
+    }
 }
 
 output "iam_output" {
-  depends_on = [ module.IAM ]
-  value =  module.IAM
+  depends_on = [ 
+    module.iam
+    ]
+  value =  module.iam
 }
 
 output "vpc_output" {
