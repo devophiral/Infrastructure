8ad13b22a24293c1ea310efe48b331d1d2ff6db6
devophiral <77823959+devophiral@users.noreply.github.com>
2023-11-10 10:34:14 +0530

    elasticbeanstalk latest files

--- modules/elasticbeanstalk/main.tf
+++ modules/elasticbeanstalk/main.tf
@@ -1,21 +1,23 @@
-provider "aws" {
-  region = var.global.aws_region
-  assume_role {
-    role_arn = aws_iam_role.elasticbeanstalk_role.arn
-  }
+data "aws_iam_role" "example" {
+  name = "ElasticBeanstalkRole"
 }
 
 # Configure Elastic Beanstalk application and environment
 resource "aws_elastic_beanstalk_application" "my_web_app" {
-  name        = "my-web-app"
-  description = "My Web App-Staging"
+  name        = "tf-test-name"
+  description = "tf-test-desc"
+
+  appversion_lifecycle {
+    service_role          = data.aws_iam_role.example.arn
+    max_count             = 128
+    delete_source_from_s3 = true   
+  }
 }
 
 resource "aws_elastic_beanstalk_environment" "my_web_app_env" {
   name                = "my-web-app-env"
   application         = aws_elastic_beanstalk_application.my_web_app.name
   solution_stack_name = "64bit Amazon Linux 2 v4.4.5 running Docker"  # Latest Amazon Linux 2 platform
-
 }
 
 output "elastic_beanstalk_url" {
