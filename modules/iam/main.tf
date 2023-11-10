1008727158dbf1661c072d64b791ab31567af49e
devophiral <77823959+devophiral@users.noreply.github.com>
2023-11-10 10:34:50 +0530

    latest files for iam

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -3,13 +3,20 @@ resource "aws_iam_role" "elasticbeanstalk_role" {
   
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = {
-        Service = "elasticbeanstalk.amazonaws.com"
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "elasticbeanstalk.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
       },
-      Action = "sts:AssumeRole"
-    }]
+      {
+        Effect = "Allow",
+        Action = "iam:GetRole",
+        Resource = "arn:aws:iam::168289737616:role/ElasticBeanstalkRole"
+      }
+    ]
   })
 }
 
@@ -17,4 +24,8 @@ resource "aws_iam_policy_attachment" "elasticbeanstalk_policy_attachment" {
   name       = "ElasticBeanstalkPolicyAttachment"
   roles      = [aws_iam_role.elasticbeanstalk_role.name]
   policy_arn = var.global.policy_arn
+}
+
+output "elasticbeanstalk_role" {
+  value = aws_iam_role.elasticbeanstalk_role.arn
 }
